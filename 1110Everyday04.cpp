cab0d690d2dda8c2c3566129b463b4618e8b7bf9
L_kingming <289437926@qq.com>
2019-11-10 11:47:11 +0800

    1110Everyday04.cpp

---
+++ 1110Everyday04.cpp
@@ -0,0 +1,67 @@
+//数组中出现次数超过一半的数字
+class Solution {
+public:
+    int MoreThanHalfNum_Solution(vector<int> numbers) {
+        int icount = 1;
+        int num = numbers[0];
+
+        int i;
+        for(i = 1; i < numbers.size() ;i++){
+            if(numbers[i] == numbers[i - 1]){
+                icount++;
+            }else{
+                if(icount <= 1){
+                    num = numbers[i];
+                }else{
+                    icount--;
+                }
+            }
+        }
+        //#include <algorithm>  begin->end中num出现的次数
+        if((numbers.size() / 2) < count(numbers.begin(), numbers.end(), num)) {
+            return num;
+        }
+        return 0;
+        }
+};
+
+
+//字符串中最长数字串:
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+
+void FindNumArr(string &s){
+    int len = 0;//最长数字串长度
+    int count = 0;//当前数字串计数
+    string tmp;
+    string max;
+    for (int i = 0; i < s.size(); ++i) {
+        if ((s[i] >= '0' && s[i] <= '9')){
+            tmp.push_back(s[i]);
+            count++;
+            if(count > len){
+                len = count;
+                max = tmp;
+            }
+        } else {
+            count = 0;
+            tmp.clear();
+            continue;
+        }
+    }
+    for(auto &i : max){
+        cout << i;
+    }
+}
+
+
+int main(){
+    string s;
+    getline(cin, s);
+    FindNumArr(s);
+    return 0;
+}
