34f89eb378620c3de17521bdf5163c6593cd6d00
L_kingming <289437926@qq.com>
2019-12-02 14:53:13 +0800

    1109BinaryTree.cpp 2nd

---
+++ 1109BinaryTree.cpp
@@ -0,0 +1,15 @@
+//
+// Created by 28943 on 2019/11/9.
+//
+
+#include "1109BinaryTree.h"
+//using namespace std;
+
+int main() {
+    km::Tree<char> tr("ABD##E##CFH##I##G##", '#');
+//    tr.pre_order();
+    tr.pre_order();
+
+
+    return 0;
+}
